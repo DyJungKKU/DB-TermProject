81d2530c40380678a0ee27019d15d185eade0b2c
Jung Day <dayever@naver.com>
2022-04-27 19:00:53 +0900

    로그인 페이지 디자인 적용

--- login.php
+++ login.php
@@ -6,8 +6,34 @@ if (isset($_SESSION['mb_id'])) {
 }
 ?>
 
-<form action="login_check.php" method="POST">
-    <input type="text" name="mb_id" id="mb_id"><br>
-    <input type="password" name="mb_password" id="mb_password">
-    <button type="submit">로그인</button>
-</form>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./css/signin.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+    <form class="form-signin" method="POST" action="login_check.php">
+      <h1 class="h3 mb-3 font-weight-normal">로그인</h1>
+      <label for="inputEmail" class="sr-only">ID</label>
+      <input type="text" id="inputEmail" name="mb_id" class="form-control" placeholder="ID" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" name="mb_password" class="form-control" placeholder="Password" required>
+
+      <button class="btn btn-lg btn-primary btn-block" type="submit">로그인</button>
+      <p class="mt-5 mb-3 text-muted">&copy; Jung.</p>
+    </form>
+  </body>
+</html>
+
