1918da6e123afe5f340ccec444e20431028cf461
Jung Day <dayever@naver.com>
2022-04-27 15:09:42 +0900

    DB 관련 추가

--- common.php
+++ common.php
@@ -12,3 +12,19 @@ function sql_query($sql) {
 function sql_fetch_array($result) {
     return mysqli_fetch_array($result);
 }
+
+function insert_point($mb_id, $point) { // 사용자 포인트 지급 함수
+    if (!is_int($point)) return; // 포인트가 정수가 아닐 경우 리턴
+    $mb = get_member($mb_id);
+    if (!$mb['mb_id']) return; // 사용자가 없을 경우 리턴
+    $newPoint = $mb['mb_point'] + $point;
+    $sql = " UPDATE `member` SET `mb_point` = '$newPoint' WHERE mb_id = '$mb_id' ";
+    sql_query($sql);
+}
+
+function get_member($mb_id) { // 사용자 SELECT 함수
+    $sql = " SELECT * FROM member WHERE mb_id = '$mb_id' ";
+    $rs = sql_query($sql);
+    $result = sql_fetch_array($rs);
+    return $result;
+}
