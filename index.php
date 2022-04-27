5f48920c09626baa77d638d3d1d374a5820212e3
Jung Day <dayever@naver.com>
2022-04-27 15:14:11 +0900

    인덱스 오류 수정

--- index.php
+++ index.php
@@ -2,10 +2,6 @@
 include_once("./_common.php");
 include_once("./_head.php");
 
-$sql = " SELECT * FROM test WHERE 1 ";
-$rs = sql_query($sql);
-$result = sql_fetch_array($rs);
-
 ?>
 <main role="main">
 
