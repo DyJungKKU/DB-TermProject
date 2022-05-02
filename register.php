343e607223bdd75743d47ecc3dce6081614f8e28
Jung Day <dayever@naver.com>
2022-05-02 14:48:40 +0900

    Register

---
+++ register.php
@@ -0,0 +1,9 @@
+<?php
+include_once("./_common.php");
+?>
+
+<form action="register_ok.php" method="POST">
+    <input type="text" name="mb_id"><br>
+    <input type="password" name="mb_password"><br>
+    <button>회원가입</button>
+</form>
---
+++ register_ok.php
@@ -0,0 +1,25 @@
+<?php
+include_once("./_common.php");
+
+$mb_id = ($_POST['mb_id']) ?? "";
+$mb_password = ($_POST['mb_password']) ?? "";
+
+echo $mb_id;
+echo "<br>";
+echo $mb_password;
+
+$getMember = get_member($mb_id);
+
+if (isset($getMember)) {
+    exit;
+    echo "True";
+} else {
+    echo "False";
+}
+
+$sql = " INSERT INTO `member` SET
+`id` = NULL,
+`mb_id` = '$mb_id',
+`mb_password` = '$mb_password',
+`mb_point` = '0' ";
+sql_query($sql);
