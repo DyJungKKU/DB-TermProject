9bc2e542d0c411223d07068be55db61f8af4473a
Jung Day <dayever@naver.com>
2022-05-30 14:31:23 +0900

    simulator commit

--- simulator.php
+++ simulator.php
@@ -65,7 +65,7 @@ input[type="number"]::-webkit-inner-spin-button {
     </nav>
     <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-        <h1 class="h2"><?php echo $symbol; ?> - <?php echo $coin['bidPrice']; ?><b style="font-size: 18px;">USDT</b> <b style="font-size: 18px;color: red">(<?php echo $coin['priceChange']; ?>)</b></h1>
+        <h1 class="h2"><?php echo $symbol; ?> - <?php echo $coin['bidPrice']; ?><b style="font-size: 18px;">USDT</b> <b style="font-size: 18px; color: <?php echo (double)$coin['priceChange'] > 0 ? "#01A66D" : "#CF304A" ?>">(<?php echo (double)$coin['priceChange'] > 0 ? "+" : "" ?><?php echo $coin['priceChange']; ?>)</b></h1>
         <div class="btn-toolbar mb-2 mb-md-0">
           <div class="btn-group mr-2">
           <div class="input-group">
